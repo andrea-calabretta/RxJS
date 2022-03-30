b9acd3d9f5f2a40ea7fc2038b8f6229b58ed27ed
andrea-calabretta <andreacalabretta0@gmail.com>
2022-03-30 15:34:55 +0200

    22 empty observable

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
-import { observable, Observable } from "rxjs";
+import { Observable } from "rxjs";
 
-const someObservable$ = new Observable<string>((subscriber) => {
+const observable$ = new Observable(subscriber => {
   console.log('Observable executed');
-  subscriber.next("Alice");
-  setTimeout(() => subscriber.next("Ben"), 2000);
-  setTimeout(() => subscriber.next("Charlie"), 4000);
 });
 
+console.log('Before subscribe');
+observable$.subscribe();
+console.log('After subscribe');
 
-console.log('Subscription 1 starts');
-someObservable$.subscribe(value => console.log('Subscription 1:', value));
-
-setTimeout(() => {
-  console.log('Subscription 1 starts');
-  someObservable$.subscribe(value => console.log('Subscription 2:', value));
-}, 1000);
 
 
