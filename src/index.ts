f4612a1ec293b4ea261820584d98e2b637bfdc47
andrea-calabretta <andreacalabretta0@gmail.com>
2022-03-31 17:42:13 +0200

    24 Asynchronous Emission - More Next Notifications

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,13 @@ import { Observable } from "rxjs";
 
 const observable$ = new Observable<string>(subscriber => {
   console.log('Observable executed');
-  subscriber.next('Alice')
+  subscriber.next('Alice');
+  subscriber.next('Ben');
+  setTimeout(() => subscriber.next('Charlie'), 2000);
 });
 
 console.log('Before subscribe');
 
-//* COMPLETE VERSION
-// const observer = {
-//   next: (value: string) => console.log(value)
-// }
-// observable$.subscribe(observer);
-
-//* SHORTER VERSION -->
-// observable$.subscribe({
-//   next: value => console.log(value)
-// });
-
-//* SHORTEST VERSION -->
 observable$.subscribe(value => console.log(value));
 
 console.log('After subscribe');
