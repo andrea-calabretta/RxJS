a8bf84419bf6db66859ab6829b801e758bad65eb
andrea-calabretta <andreacalabretta0@gmail.com>
2022-03-31 17:20:04 +0200

    23 Synchronous Emission - Next Notification

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import { Observable } from "rxjs";
 
-const observable$ = new Observable(subscriber => {
+const observable$ = new Observable<string>(subscriber => {
   console.log('Observable executed');
+  subscriber.next('Alice')
 });
 
 console.log('Before subscribe');
-observable$.subscribe();
+
+//* COMPLETE VERSION
+// const observer = {
+//   next: (value: string) => console.log(value)
+// }
+// observable$.subscribe(observer);
+
+//* SHORTER VERSION -->
+// observable$.subscribe({
+//   next: value => console.log(value)
+// });
+
+//* SHORTEST VERSION -->
+observable$.subscribe(value => console.log(value));
+
 console.log('After subscribe');
 
 
