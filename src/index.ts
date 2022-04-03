d13c2a7dc8ec977b663ae9dbc91d222cc04be3da
andrea-calabretta <andreacalabretta0@gmail.com>
2022-04-03 19:14:28 +0200

    28 Order

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ const observable$ = new Observable<string>((subscriber) => {
   console.log('Observable executed');
   subscriber.next('Alice');
   subscriber.next('Ben');
-  setTimeout(() => subscriber.next('Charlie'), 2000);
-  setTimeout(() => subscriber.error(new Error('Failure')), 4000);
+  setTimeout(() => subscriber.error(new Error('Failure')), 2000);
+  setTimeout(() => {
+    subscriber.next('Charlie');
+    subscriber.complete();
+  }, 4000);
   return () => {
     console.log("Teardown logic");
   };
@@ -13,15 +16,6 @@ const observable$ = new Observable<string>((subscriber) => {
 
 console.log("Before subscribe");
 
-//EXPLICIT VERSION
-// const observer = {
-//   next: (value: string) => console.log(value),
-//   error: (err: { message: string; }) => console.log(err.message),
-//   complete: () => console.log('Completed!')
-// }
-// observable$.subscribe(observer);
-
-//SHORTER VERSION
 observable$.subscribe({
   next: (value) => console.log(value),
   error: err => console.log(err.message),
