2cc81eedca45b49bb62aeb6076af0ea107c40b67
andrea-calabretta <andreacalabretta0@gmail.com>
2022-04-09 09:23:19 +0200

    38 From promise rejected to Observable

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { from } from "rxjs";
 
 
 const somePromise = new Promise((resolve, reject) => {
-  resolve('Resolved!');
+  // resolve('Resolved!');
+  reject('Rejected!')
 });
 
 const observableFromPromise$ = from(somePromise);
 
 observableFromPromise$.subscribe({
   next: value => console.log(value),
+  error: err => console.log('Error:', err),
   complete: () => console.log('Completed'),
 })
