8b72f04d56ba931afac82cb0611d4d424551af27
andrea-calabretta <andreacalabretta0@gmail.com>
2022-04-22 14:55:17 +0200

    52 debounceTime

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
-import { filter, map, of, tap } from "rxjs";
+import { debounceTime, fromEvent, map } from "rxjs";
 
-of(1, 7, 3, 6, 2).pipe(
-  tap(value => console.log('Spy: ', value)),
-  map(value => value * 2),
-  filter(value => value > 5),
-).subscribe(value => console.log('Output:', value));
+const sliderInput = document.querySelector('input#slider');
 
-// of(1, 7, 3, 6, 2).pipe(
-//   filter(value => value > 5),
-//   map(value => value * 2),
-//   tap(value => console.log('Spy: ', value)),
-// ).subscribe(value => console.log('Output:', value));
+fromEvent(sliderInput, 'input').pipe(
+  debounceTime(2000),
+  map(event => (<HTMLInputElement>event.target)['value'])
+).subscribe(value => console.log(value));
 
-
-// of(1, 7, 3, 6, 2).pipe(
-//   filter(value => value > 5),
-//   map(value => value * 2),
-//   tap({
-//     next : value => console.log('Spy: ', value),
-//     complete : () => console.log('Spy: complete'),
-//   }),
-// ).subscribe(value => console.log('Output:', value));
