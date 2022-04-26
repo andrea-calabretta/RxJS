2d4b8371b3f93c78f7e27843326959213fa67535
andrea-calabretta <andreacalabretta0@gmail.com>
2022-04-26 15:20:26 +0200

    56 flattening operator dynamic

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-import { concatMap, Observable, of } from "rxjs";
+import { concatMap, fromEvent, map } from "rxjs";
+import { ajax } from 'rxjs/ajax';
 
-const source$ = new Observable(subscriber => {
-  setTimeout(() => subscriber.next('A'), 2000);
-  setTimeout(() => subscriber.next('B'), 5000);
-});
+const endpointInput : HTMLInputElement = document.querySelector('input#endpoint');
+const fetchButton = document.querySelector('button#fetch');
 
-console.log('App has started');
-
-// source$.subscribe(value => console.log(value));
-
-source$.pipe(
-  concatMap(value => of(1,2))
-).subscribe(value => console.log(value));
-
-
-
+fromEvent(fetchButton, 'click').pipe(
+  map( () => endpointInput.value ),
+  concatMap(value => 
+    ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+  )
+).subscribe(
+  value => console.log(value)
+);
 
