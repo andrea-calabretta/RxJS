0ae1d0970cdabffa90a2c317af97e755ceef1f36
andrea-calabretta <andreacalabretta0@gmail.com>
2022-04-27 10:36:36 +0200

    58 correct error handlig

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { concatMap, fromEvent, map } from "rxjs";
+import { catchError, concatMap, EMPTY, fromEvent, map } from "rxjs";
 import { ajax } from 'rxjs/ajax';
 
 const endpointInput : HTMLInputElement = document.querySelector('input#endpoint');
@@ -7,9 +7,13 @@ const fetchButton = document.querySelector('button#fetch');
 fromEvent(fetchButton, 'click').pipe(
   map( () => endpointInput.value ),
   concatMap(value => 
-    ajax(`https://random-data-api.com/api/${value}/random_${value}`)
-  )
-).subscribe(
-  value => console.log(value)
-);
+    ajax(`https://random-data-api.com/api/${value}/random_${value}`).pipe(
+      catchError(() => EMPTY)
+    )
+  ),
+).subscribe({
+  next : value => console.log(value),
+  error: err => console.log('Error: ', err),
+  complete: () => console.log('Completed')
+});
 
