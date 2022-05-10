2b80454dc9b311f3012cdcf82e04acccfcefe292
andrea-calabretta <andreacalabretta0@gmail.com>
2022-05-10 20:27:03 +0200

    41 interval

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,26 @@ import { interval, Observable } from "rxjs";
 
 console.log('App started');
 
-const interval$ = new Observable<number>(subscriber => {
-  let counter = 0;
-
-  const intervalId = setInterval(() => {
-    console.log('new interval');
-    subscriber.next(counter++);
-  }, 1000);
-
-  return () => clearInterval(intervalId);
-})
-
-const subscription = interval$.subscribe({
+const subscription = interval(1000).subscribe({
   next: value => console.log(value),
-  complete: () => console.log('Complete!'),
+  complete: () => console.log('Completed!')
 })
+
+// const interval$ = new Observable<number>(subscriber => {
+//   let counter = 0;
+
+//   const intervalId = setInterval(() => {
+//     console.log('new interval');
+//     subscriber.next(counter++);
+//   }, 1000);
+
+//   return () => clearInterval(intervalId);
+// })
+
+// const subscription = interval$.subscribe({
+//   next: value => console.log(value),
+//   complete: () => console.log('Complete!'),
+// })
 
 setTimeout(() => {
   subscription.unsubscribe();
