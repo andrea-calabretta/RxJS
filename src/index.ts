c3a75efaf95c910d2a46e091bc17cde0cebcf1e8
andrea-calabretta <andreacalabretta0@gmail.com>
2022-05-10 20:29:14 +0200

    41 interval

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,6 @@ const subscription = interval(1000).subscribe({
   complete: () => console.log('Completed!')
 })
 
-// const interval$ = new Observable<number>(subscriber => {
-//   let counter = 0;
-
-//   const intervalId = setInterval(() => {
-//     console.log('new interval');
-//     subscriber.next(counter++);
-//   }, 1000);
-
-//   return () => clearInterval(intervalId);
-// })
-
-// const subscription = interval$.subscribe({
-//   next: value => console.log(value),
-//   complete: () => console.log('Complete!'),
-// })
 
 setTimeout(() => {
   subscription.unsubscribe();
