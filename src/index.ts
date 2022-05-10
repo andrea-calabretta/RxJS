60a746e34f89ffe6a0f1056a6497370acd27960a
andrea-calabretta <andreacalabretta0@gmail.com>
2022-05-10 20:23:26 +0200

    40 timer self implementation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-import { timer } from "rxjs";
+import { Observable, timer } from "rxjs";
 
 console.log('App Started');
 
-timer(2000).subscribe({
-  next: value => console.log(value),
-  complete: () => console.log('Completed')
+// timer(2000).subscribe({
+//   next: value => console.log(value),
+//   complete: () => console.log('Completed')
+// })
+
+const timer$ = new Observable<number>(subscriber => {
+  setTimeout(() => {
+    subscriber.next(0);
+    subscriber.complete();
+  }, 2000);
 })
+
+timer$.subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('Completed!')
+});
