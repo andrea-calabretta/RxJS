bd29bf3094820911905a8496f54ff836b3fc4d17
andrea-calabretta <andreacalabretta0@gmail.com>
2022-05-10 20:19:21 +0200

    40 timer

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,8 @@
-import { fromEvent, Observable } from "rxjs";
+import { timer } from "rxjs";
 
-const triggerButton = document.querySelector('button#trigger');
+console.log('App Started');
 
-// fromEvent<MouseEvent>(triggerButton, 'click').subscribe(
-//   event => console.log(event.type, event.x, event.y)
-// );
-
-const triggerClick$ = new Observable<MouseEvent>(subscriber => {
-  const clickHandlerFn = (event: MouseEvent) => {
-    console.log('Event callback executed');
-    subscriber.next(<MouseEvent>event);
-  };
-  triggerButton.addEventListener('click', clickHandlerFn);
-  
-  return () => {
-    triggerButton.removeEventListener('click', clickHandlerFn )
-  }
-});
-
-const subscription = triggerClick$.subscribe(
-  event => console.log(event.type, event.x, event.y)
-);
-
-setTimeout(() => {
-  console.log('Unsubscribe');
-  subscription.unsubscribe();
-}, 5000);
+timer(2000).subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('Completed')
+})
